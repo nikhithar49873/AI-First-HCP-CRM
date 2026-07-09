f7f29e4f0d9dfa5f0ebb742bfb06c45e234a8b7b
nikhithar49873 <nikhithar140@gmail.com>
2026-07-09 19:15:45 +0530

    Completed CRUD operations for Interaction module

--- backend/app/api/interaction.py
+++ backend/app/api/interaction.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from typing import List
 
 from app.database.connection import get_db
 from app.schemas.interaction import (
@@ -7,7 +8,13 @@ from app.schemas.interaction import (
     InteractionResponse,
 )
 
-from app.services.interaction_service import create_interaction
+from app.services.interaction_service import (
+    create_interaction,
+    get_all_interactions,
+    get_interaction,
+    update_interaction,
+    delete_interaction
+)
 
 router = APIRouter(
     prefix="/interactions",
@@ -21,3 +28,51 @@ def add_interaction(
     db: Session = Depends(get_db)
 ):
     return create_interaction(db, interaction)
+@router.get("/", response_model=List[InteractionResponse])
+def get_interactions(
+    db: Session = Depends(get_db)
+):
+    return get_all_interactions(db)
+
+
+@router.get("/{interaction_id}",
+            response_model=InteractionResponse)
+def get_single_interaction(
+    interaction_id: int,
+    db: Session = Depends(get_db)
+):
+    return get_interaction(
+        db,
+        interaction_id
+    )
+
+
+@router.put("/{interaction_id}",
+            response_model=InteractionResponse)
+def edit_interaction(
+    interaction_id: int,
+    interaction: InteractionCreate,
+    db: Session = Depends(get_db)
+):
+
+    return update_interaction(
+        db,
+        interaction_id,
+        interaction
+    )
+
+
+@router.delete("/{interaction_id}")
+def remove_interaction(
+    interaction_id: int,
+    db: Session = Depends(get_db)
+):
+
+    delete_interaction(
+        db,
+        interaction_id
+    )
+
+    return {
+        "message": "Interaction deleted successfully"
+    }
--- backend/app/services/interaction_service.py
+++ backend/app/services/interaction_service.py
@@ -24,3 +24,56 @@ def create_interaction(db: Session, interaction: InteractionCreate):
     db.refresh(new_interaction)
 
     return new_interaction
+def get_all_interactions(db: Session):
+    return db.query(Interaction).all()
+
+
+def get_interaction(db: Session, interaction_id: int):
+    return db.query(Interaction).filter(
+        Interaction.id == interaction_id
+    ).first()
+
+
+def update_interaction(
+    db: Session,
+    interaction_id: int,
+    interaction: InteractionCreate
+):
+    existing = db.query(Interaction).filter(
+        Interaction.id == interaction_id
+    ).first()
+
+    if existing:
+
+        existing.doctor_name = interaction.doctor_name
+        existing.hospital = interaction.hospital
+        existing.specialization = interaction.specialization
+        existing.interaction_date = interaction.interaction_date
+        existing.interaction_type = interaction.interaction_type
+        existing.discussion = interaction.discussion
+        existing.products = interaction.products
+        existing.follow_up_date = interaction.follow_up_date
+        existing.notes = interaction.notes
+
+        db.commit()
+        db.refresh(existing)
+
+    return existing
+
+
+def delete_interaction(
+    db: Session,
+    interaction_id: int
+):
+
+    interaction = db.query(
+        Interaction
+    ).filter(
+        Interaction.id == interaction_id
+    ).first()
+
+    if interaction:
+        db.delete(interaction)
+        db.commit()
+
+    return interaction

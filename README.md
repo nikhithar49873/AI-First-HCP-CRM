310b3c225f125e16141a35c4455790d1004b3d8e
nikhithar49873 <nikhithar140@gmail.com>
2026-07-09 16:27:23 +0530

    Added frontend, README and gitignore

---
+++ README.md
@@ -0,0 +1,17 @@
+# AI-First HCP CRM
+
+## Tech Stack
+
+- React
+- Redux Toolkit
+- FastAPI
+- MySQL
+- LangGraph
+- Groq (Gemma2-9B)
+
+## Modules
+
+- Dashboard
+- Log Interaction
+- Chat Interface
+- Interaction History
---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>Welcome!</h1>
+
+      <p>This is our Healthcare CRM Project.</p>
+    </div>
+  );
+}
+
+export default App;
